75d70b431ade5044fd153ff8f1aedd441cbb25d1
ZIN MYO <overmidnight12@gmail.com>
2019-12-05 07:59:46 -0800

    my changes

--- files/passlogy_demo.js
+++ files/passlogy_demo.js
@@ -9,30 +9,33 @@ $(document).ready(function(){
 //Authentication
 var j = 0;
 function login(){
-var passwd = $("p#0").html();
-	passwd += $("p#42").html();
+var 
+	passwd = $("p#0").html();
+	passwd += $("p#5").html();
 	passwd += $("p#10").html();
 	passwd += $("p#15").html();
 	passwd += $("p#28").html();
 	passwd += $("p#25").html();
 	passwd += $("p#22").html();
-	passwd += $("p#19").html();
+	passwd += $("p#32").html();
 	var inpasswd = $(":text").val();
 	if(passwd==inpasswd){
 		location.href="menu.html";
 	}else if(j==0){
 		$("#info").html("<div class='alert alert-danger' role='alert' style='margin-bottom: 0.3rem;'><i class='fas fa-exclamation'></i> The password is incorrect.</div>");
 	    j++;
-	}else{
+	}
+
+	else{
 		$("#info").html("<div class='alert alert-danger' role='alert'><i class='fas fa-exclamation'></i> The password is incorrect.<br><b>The correct password has been highlighted above</b>. Please enter it in the password field.<br><small>*You must enter the correct password to proceed.</small></div>");
 		$("p#0").css('background-color','#FFBCBC');
-	    $("p#42").css('background-color','#FFBCBC');
+	    $("p#5").css('background-color','#FFBCBC');
 	    $("p#10").css('background-color','#FFBCBC');
 	    $("p#15").css('background-color','#FFBCBC');
 	    $("p#28").css('background-color','#FFBCBC');
 	    $("p#25").css('background-color','#FFBCBC');
 	    $("p#22").css('background-color','#FFBCBC');
-	    $("p#19").css('background-color','#FFBCBC');
+	    $("p#32").css('background-color','#FFBCBC');
 	    j++;
 	}
 };
